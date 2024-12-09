03133620f04813f55df81cec3cc0a1395766df33
Simonhil <sihi@outlook.de>
2024-12-09 11:12:00 +0100

    small changes to peg_insertion example

--- examples/async_peg_insert_drq_noG/run_actor.sh
+++ examples/async_peg_insert_drq_noG/run_actor.sh
@@ -6,7 +6,7 @@ python async_drq_randomized.py "$@" \
     --env FrankaPegInsert-Vision-v0 \
     --exp_name=serl_dev_drq_rlpd10demos_peg_insert_random_resnet \
     --seed 0 \
-    --random_steps 0 \
+    --random_steps 0\
     --training_starts 200 \
     --encoder_type resnet-pretrained \
-    --demo_path /home/shilber/delete/peg_insert__demos_2024-10-18_09-42-36.pkl\
+    --demo_path /home/shilber/delete/20_hex_front_wrist.pkl\
--- examples/async_peg_insert_drq_noG/run_learner.sh
+++ examples/async_peg_insert_drq_noG/run_learner.sh
@@ -1,15 +1,15 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.2 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.9 && \
 python async_drq_randomized.py "$@" \
     --learner \
     --env FrankaPegInsert-Vision-v0 \
     --exp_name=serl_dev_drq_rlpd10demos_peg_insert_random_resnet_097 \
     --seed 0 \
     --random_steps 1000 \
-    --training_starts 5\
+    --training_starts 200\
     --critic_actor_ratio 4 \
     --eval_period 2000 \
     --encoder_type resnet-pretrained \
-    --demo_path /home/shilber/delete/peg_insert__demos_2024-10-18_09-42-36.pkl\
-    --checkpoint_period 10000 \
+    --demo_path /home/shilber/delete/20_hex_front_wrist.pkl\
+    --checkpoint_period 3000 \
     --checkpoint_path /home/shilber/delete/test1\
